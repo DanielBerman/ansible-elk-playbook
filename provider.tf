856caa57bf4dd44214f0c58e33bddb9ab7e6205c
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-08 09:58:16 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,10 +16,31 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+resource "aws_security_group" "test_sg" {
+  name = "test_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outgoing traffic to anywhere.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
    ami           = "ami-0287acb18b6d8efff"
    instance_type = "t2.micro"
+   key_name        = "jenkinskey"
+   security_groups = ["${aws_security_group.test_sg.name}"]
    provisioner "local-exec" {
-   command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${aws_instance.example.public_dns},' site.yml"
+   command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ./jenkinskey.pem -i '${aws_instance.example.public_dns},' site.yml"
  }
 }
