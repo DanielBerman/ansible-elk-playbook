e1506cdae20733efcd42bb391519acc33e85f5f9
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-08 17:16:46 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 resource "aws_key_pair" "sshkey" {
   key_name   = "SSH-Key"
-  public_key = file("var.public_key_path")
+  public_key = file(var.public_key_path)
   }
 
 resource "aws_security_group" "test_sg" {
@@ -52,8 +52,8 @@ resource "aws_instance" "example" {
     connection {
       type        = "ssh"
       user        = var.ssh_user
-      private_key = file("var.private_key_path")
-      host        = self.private_ip
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
       }
     }
    provisioner "local-exec" {
