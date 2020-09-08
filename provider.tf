7defde7c9203d8ad92e10a8f8c9eed2731056171
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-08 16:05:44 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,8 +16,8 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-resource "aws_key_pair" "sshkey" {
-  key_name   = "${var.stack}-key"
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "SSH-Key"
   public_key = "file(var.public_key_path)"
 }
 
@@ -41,15 +41,16 @@ resource "aws_security_group" "test_sg" {
 }
 
 resource "aws_instance" "example" {
-   ami           = "ami-0287acb18b6d8efff"
-   instance_type = "t2.micro"
-   security_groups = ["${aws_security_group.test_sg.name}"]
+   key_name         = aws_key_pair.ssh-key.key_name
+   ami              = "ami-0287acb18b6d8efff"
+   instance_type    = "t2.micro"
+   security_groups  = ["${aws_security_group.test_sg.name}"]
   
  
 
     
   
    provisioner "local-exec" {
-   command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${aws_instance.example.public_dns},' site.yml"
+   command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.private_key_path} -i '${aws_instance.example.public_dns},' site.yml"
  }
 }
