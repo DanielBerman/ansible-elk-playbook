395cbfc9730669cd114dd0b6442aec8a4fa418b4
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-08 23:14:50 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 resource "aws_key_pair" "sshkey" {
   key_name   = "SSH-Key4"
-  public_key = file("/home/ubuntu/.ssh/id_rsa/key.pub")
+  public_key = file("/home/ubuntu/usr/bin/key.pub")
   }
 
 resource "aws_security_group" "test_sg" {
@@ -59,7 +59,7 @@ resource "aws_instance" "example" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("/home/ubuntu/.ssh/id_rsa/key")
+    private_key = file("/home/ubuntu/usr/bin/key")
     host        = self.public_ip
   }
    provisioner "local-exec" {
