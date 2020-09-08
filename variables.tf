9c128703c4b754bd1db1a0c4495e9709ef672f38
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-08 23:53:05 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "public_key_path" {
   description = "Path to the public SSH key you want to bake into the instance."
-  default     = "/var/lib/jenkins/terraform.pub"
+  default     = "/usr/bin/key.pub"
 }
 
 variable "private_key_path" {
   description = "Path to the private SSH key, used to access the instance."
-  default     = "/var/lib/jenkins/terraform"
+  default     = "/usr/bin/key"
 }
 
 variable "project_name" {
