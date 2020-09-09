84a0aa3f94d34142345c8028c743ab3598b396e4
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-09 14:58:21 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-variable "key_name" {default="my-key10"}
+variable "key_name" {default="my-key11"}
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
