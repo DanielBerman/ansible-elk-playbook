9a826cb4ee09eda0526de3e1f3426ac331254e85
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-09 10:13:42 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -22,7 +22,7 @@ resource "tls_private_key" "example" {
   rsa_bits  = 4096
 }
 resource "aws_key_pair" "generated_key" {
-  key_name   = "${var.key_name}"
+  key_name   = var.key_name
   public_key = "${tls_private_key.example.public_key_openssh}"
 }
 
