929695d365ae5666719837e827bf87f57243418a
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-09 13:30:26 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-variable "key_name" {default="my-key5"}
+variable "key_name" {default="my-key6"}
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -77,6 +77,6 @@ resource "aws_instance" "example" {
  }
    provisioner "local-exec" {
   # command = "sleep 120; ansible-playbook host_key_checking=false -u ubuntu --private-key ${var.private_key_path} -i '${aws_instance.example.public_dns},' site.yml"
-   command = "sleep 120; ansible-playbook -e host_key_checking=False -u ubuntu -i '${aws_instance.example.public_dns},' --private-key ${tls_private_key.example.private_key_pem} site.yml"
+   command = "sleep 120; ansible-playbook -e host_key_checking=False -u ubuntu -i '${aws_instance.example.public_dns},' --private-key ${tls_private_key.example.private_key_pem} ./site.yml -b"
  }
 }
