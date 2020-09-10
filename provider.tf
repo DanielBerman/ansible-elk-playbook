b382497d1f610dd8ac71b3b90e3dbbfc0d842db0
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-11 00:15:49 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "test_sg" {
 resource "aws_instance" "example" {
   # key_name         = aws_key_pair.terraform-ansible.key_name
   # key_name         = "jenkinskey"
-    key_name        = "${var.private_key_path}"
+    key_name        = var.private_key_path
    ami              = "ami-0287acb18b6d8efff"
    instance_type    = "t2.micro"
    security_groups  = ["${aws_security_group.test_sg.name}"]
